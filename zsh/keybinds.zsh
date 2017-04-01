fd6909fdb4d97b5de742b0582b8dfbaf51f837fc
Herringway <elpenguino@gmail.com>
2017-04-01 19:06:55 -0300

    move keybind-related conf to keybinds.zsh where it belongs

--- zsh/keybinds.zsh
+++ zsh/keybinds.zsh
@@ -27,3 +27,17 @@ key[PageDown]=${terminfo[knp]}
 [[ -n "${key[Right]}"    ]]  && bindkey  "${key[Right]}"    forward-char
 [[ -n "${key[PageUp]}"   ]]  && bindkey  "${key[PageUp]}"   beginning-of-buffer-or-history
 [[ -n "${key[PageDown]}" ]]  && bindkey  "${key[PageDown]}" end-of-buffer-or-history
+
+# Finally, make sure the terminal is in application mode, when zle is
+# active. Only then are the values from $terminfo valid.
+if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
+    function zle-line-init () {
+        printf '%s' "${terminfo[smkx]}"
+    }
+    function zle-line-finish () {
+        printf '%s' "${terminfo[rmkx]}"
+    }
+    zle -N zle-line-init
+    zle -N zle-line-finish
+fi
+
--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -15,17 +15,4 @@ REPORTTIME=10 #if command takes longer than 10s, print stats
 export EDITOR=nano
 export GPG_TTY=`tty`
 
-# Finally, make sure the terminal is in application mode, when zle is
-# active. Only then are the values from $terminfo valid.
-if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
-    function zle-line-init () {
-        printf '%s' "${terminfo[smkx]}"
-    }
-    function zle-line-finish () {
-        printf '%s' "${terminfo[rmkx]}"
-    }
-    zle -N zle-line-init
-    zle -N zle-line-finish
-fi
-
 [ -f ${HOME}/.travis/travis.sh ] && source ${HOME}/.travis/travis.sh
