0f6cd62caae007c7bd12165df42a5e21307ccdda
Herringway <elpenguino@gmail.com>
2017-04-10 22:06:17 -0300

    zsh: ensure vcs info gets updated before next prompt is displayed

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -19,16 +19,22 @@ function update_git_vars {
   [[ -n $vcs_info_msg_0_ ]] && psvar[1]="$fg[red][$fg[white]$vcs_info_msg_0_$fg[red]]"
 }
 
-#updates vcs info when appropriate command is executed
+#prepares to update vcs info when appropriate command is executed
 function preexec_update_git_vars() {
-    case "$2" in
-        git*|hub*|gh*|stg*)
-        update_git_vars
-        ;;
-    esac
+  case "$2" in
+    git*|hub*|gh*|stg*)
+    GIT_VARS_NEED_UPDATE=1
+    ;;
+  esac
+}
+function precmd_update_git_vars() {
+  if [ -n "$GIT_VARS_NEED_UPDATE" ]; then
+    update_git_vars
+    unset GIT_VARS_NEED_UPDATE
+  fi
 }
 
 #update vcs info when dir changes and when a command is about to be executed
 add-zsh-hook chpwd update_git_vars
 add-zsh-hook preexec preexec_update_git_vars
-
+add-zsh-hook precmd precmd_update_git_vars
