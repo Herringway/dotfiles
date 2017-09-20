7a5fcee7680c6d4fb127cf5237acaf7a1011485d
Cameron Ross <elpenguino@gmail.com>
2017-09-20 16:12:34 -0300

    add nano settings, don't let installer overwrite existing settings

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
+#!/bin/sh
 INSTALLDIR=`realpath -s $(dirname ${0}$)`
-echo export ZSHRCDIR=\"${INSTALLDIR}/zsh/\" >~/.zshrc
-echo source \"\${ZSHRCDIR}/main.zsh\" >>~/.zshrc
+if [ -a ~/.zshrc ]; then
+	echo "A .zshrc is already present! Not replacing."
+else
+	echo export ZSHRCDIR=\"${INSTALLDIR}/zsh/\" >~/.zshrc
+	echo source \"\${ZSHRCDIR}/main.zsh\" >>~/.zshrc
+fi
+if [ -a ~/.nanorc ]; then
+	echo "A .nanorc is already present! Not replacing."
+else
+	ln -s "${INSTALLDIR}/nanorc" ~/.nanorc
+fi
