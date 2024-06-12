57b68de0555f42c966eb266be01cd7d57b8d04fe
Cameron Ross <elpenguino@gmail.com>
2024-06-12 12:38:45 -0300

    make powershell prompt a little prettier

--- Powershell/powershell.ps1
+++ Powershell/powershell.ps1
@@ -1,3 +1,15 @@
 & "$PSScriptRoot\LoadModules.ps1"
+function global:PromptWriteErrorInfo() {
+    if ($global:GitPromptValues.DollarQuestion) { return }
+
+    if ($global:GitPromptValues.LastExitCode) {
+        " `e[31m(" + $global:GitPromptValues.LastExitCode + ")`e[0m"
+    }
+    else {
+        " `e[31m!`e[0m"
+    }
+}
+$global:GitPromptSettings.DefaultPromptBeforeSuffix.Text = '$(PromptWriteErrorInfo)'
+$global:GitPromptSettings.DefaultPromptPath.ForegroundColor = 0x008CFF
 Set-Alias subl 'C:\Program Files\Sublime Text\subl.exe'
 [Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
